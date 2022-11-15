d903ca097528aae92d59328c46337e7e861ecc2b
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-11-15 17:15:27 +0100

    lista_for

---
+++ main.js
@@ -0,0 +1,28 @@
+/* Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).Ricordiamoci:
+
+     di inizializzare la variabile di contatore prima dell’inizio del ciclo while
+     di incrementare il contatore, o comunque di inserire una condizione di uscita, all’interno ciclo while
+    di partire sempre semplici prima con i log e poi passiamo all’output in pagina
+    sta mano po esse fero e po esse piuma 
+
+Bonus;
+
+    l’utente può inserire al volo elementi alla lista. */
+
+    let lista = ['mela', 'banana', 'sale', 'pepe'];
+    
+
+    for (let i = 0; i < lista.length; i++) {
+        
+       let listItem = lista[i] ;
+       
+        alert(listItem);
+        
+        
+        
+    }
+
+    alert(lista);
+
+    
+
